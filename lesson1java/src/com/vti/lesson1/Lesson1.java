934794c4163cba728deb3935af04412e76ea3333
hoanganh9091 <149076439+hoanganh9091@users.noreply.github.com>
2023-11-04 23:16:38 +0900

    la
    la

--- lesson1java/src/com/vti/lesson1/Lesson1.java
+++ lesson1java/src/com/vti/lesson1/Lesson1.java
@@ -2,6 +2,6 @@ package com.vti.lesson1;
 
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("Hello　World");
+        System.out.println("Hello World");
     }
 }
